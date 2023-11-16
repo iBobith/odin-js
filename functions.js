80ca81c4d42d79007c55d72fe1a981cc088f4aca
Bobith <jansons.kris@gmail.com>
2023-11-16 12:28:32 +0200

    Started learning more about functions

---
+++ functions.js
@@ -0,0 +1,8 @@
+// Pretty much anytime you make use of a JavaScript structure that features a pair of parentheses — () — and you're not
+// using a common built-in language structure like a for loop, while or do...while loop, or if...else statement, 
+// you are making use of a function.
+
+//Built-in browser functions
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
